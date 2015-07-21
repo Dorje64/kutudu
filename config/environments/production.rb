e45fb6bb478013ef26b43856fc5900d6e2f245bb
Dorje <ku.dorjee@gmail.com>
2015-07-21 18:46:53 +0545

    commented vestigial links

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,29 +45,29 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
-  # when problems arise.
-  config.log_level = :debug
-
-  config.action_mailer.default_url_options = { :host => 'kutudu.herokuapp.com' }
-
-  Rails.application.routes.default_url_options[:host] = 'kutudu.herokuapp.com'
-
-
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
-
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: ENV["GMAIL_DOMAIN"],
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: ENV["GMAIL_USERNAME"],
-      password: ENV["GMAIL_PASSWORD"]
-  }
+  # # when problems arise.
+  # config.log_level = :debug
+  #
+  # config.action_mailer.default_url_options = { :host => 'kutudu.herokuapp.com' }
+  #
+  # Rails.application.routes.default_url_options[:host] = 'kutudu.herokuapp.com'
+  #
+  #
+  #
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.default :charset => "utf-8"
+  #
+  # config.action_mailer.smtp_settings = {
+  #     address: "smtp.gmail.com",
+  #     port: 587,
+  #     domain: ENV["GMAIL_DOMAIN"],
+  #     authentication: "plain",
+  #     enable_starttls_auto: true,
+  #     user_name: ENV["GMAIL_USERNAME"],
+  #     password: ENV["GMAIL_PASSWORD"]
+  # }
 
 
 
