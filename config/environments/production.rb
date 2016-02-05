15c9ffb66eb2ac1ba6928eca3dc6a0ab8ec56c0d
Dorje <ku.dorjee@gmail.com>
2016-02-05 23:57:12 +0545

    error handling

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,5 +86,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'kutudo.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'kutudu.herokuapp.com' }
 end
